964b924f3633aa21f8efad5bdb2c608a6077bfa7
Ashu Gupta <ashugpt1812@gmail.com>
2023-07-27 20:08:51 +0530

    added express with graphql

--- server/dist/index.js
+++ server/dist/index.js
@@ -1,9 +1,32 @@
+import express from "express";
+import cors from 'cors';
+import dotenv from "dotenv";
 import { ApolloServer } from '@apollo/server';
-import { startStandaloneServer } from '@apollo/server/standalone';
+import { expressMiddleware } from "@apollo/server/express4";
 import { resolvers } from "./graphql/resolvers/resolvers.js";
 import { typeDefs } from "./graphql/typeDefs/typeDefs.js";
-const apolloserver = new ApolloServer({ typeDefs, resolvers });
-const { url } = await startStandaloneServer(apolloserver, {
-    listen: { port: 4000 },
-});
-console.log(`🚀  Server ready at: ${url}`);
+dotenv.config();
+const app = express();
+const port = 1337;
+const bootstrapServer = async () => {
+    const apolloserver = new ApolloServer({ typeDefs, resolvers });
+    await apolloserver.start();
+    app.use(cors());
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+    app.use("/graphql", expressMiddleware(apolloserver));
+    app.get("/", (req, res) => {
+        res.send("Response received from Home route");
+    });
+    app.listen(port, () => {
+        console.log(`🚀  Express ready at http://localhost:${port}`);
+        console.log(`🚀  Graphql ready at http://localhost:${port}/graphql`);
+    });
+};
+bootstrapServer();
+// import { startStandaloneServer } from '@apollo/server/standalone';
+// const apolloserver = new ApolloServer({ typeDefs, resolvers });
+// const { url } = await startStandaloneServer(apolloserver, {
+//     listen: { port: port },
+// });
+// console.log(`🚀  Apollo Server ready at: ${url}`);
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,42 @@
+import express from "express";
+import cors from 'cors';
+import dotenv from "dotenv";
 import { ApolloServer } from '@apollo/server';
-import { startStandaloneServer } from '@apollo/server/standalone';
+import { expressMiddleware } from "@apollo/server/express4";
 import { resolvers } from "./graphql/resolvers/resolvers.js";
 import { typeDefs } from "./graphql/typeDefs/typeDefs.js";
+dotenv.config();
+const app = express();
+const port = 1337;
 
-const apolloserver = new ApolloServer({ typeDefs, resolvers });
+const bootstrapServer = async () => {
+    const apolloserver = new ApolloServer({ typeDefs, resolvers });
+    await apolloserver.start();
 
-const { url } = await startStandaloneServer(apolloserver, {
-    listen: { port: 4000 },
-});
-console.log(`🚀  Server ready at: ${url}`);
+    app.use(cors());
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+    app.use("/graphql", expressMiddleware(apolloserver));
+
+    app.get("/", (req, res) => {
+        res.send("Response received from Home route");
+    });
+
+    app.listen(port, () => {
+        console.log(`🚀  Express ready at http://localhost:${port}`);
+        console.log(`🚀  Graphql ready at http://localhost:${port}/graphql`);
+    })
+
+}
+
+bootstrapServer();
+
+// standalone graphql server
+// import { startStandaloneServer } from '@apollo/server/standalone';
+// import { resolvers } from "./graphql/resolvers/resolvers.js";
+// import { typeDefs } from "./graphql/typeDefs/typeDefs.js";
+// const apolloserver = new ApolloServer({ typeDefs, resolvers });
+// const { url } = await startStandaloneServer(apolloserver, {
+//     listen: { port: 4000 },
+// });
+// console.log(`🚀  Apollo Server ready at: ${url}`);
