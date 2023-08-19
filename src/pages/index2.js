150a375b3cdf4e386c9d11b0508a8de45e73c1ba
Ashu Gupta <ashugpt1812@gmail.com>
2023-08-19 18:40:31 +0530

    nextjs first commit

---
+++ src/pages/index2.js
@@ -0,0 +1,10 @@
+// pages/index.js (or any other component file)
+import MainWindow from "./views/mainWindow";
+
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center h-[100vh]">
+      <MainWindow />
+    </div>
+  );
+}
