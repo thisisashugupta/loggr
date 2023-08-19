af4f78ac10d5372f136de2651cd07db2be605242
Ashu Gupta <ashugpt1812@gmail.com>
2023-08-19 19:30:49 +0530

    changed account route to homie

--- src/app/auth/callback/route.js
+++ src/app/auth/callback/route.js
@@ -11,5 +11,5 @@ export async function GET(req) {
     await supabase.auth.exchangeCodeForSession(code);
   }
 
-  return NextResponse.redirect(new URL("/account", req.url));
+  return NextResponse.redirect(new URL("/homie", req.url));
 }
