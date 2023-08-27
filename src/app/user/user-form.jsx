2c6e5f9e758b7a34cd5a937309c6a6b348d9f1f9
Ashu Gupta <ashugpt1812@gmail.com>
2023-08-27 22:47:29 +0530

    cleaned user table

--- src/app/user/user-form.jsx
+++ src/app/user/user-form.jsx
@@ -5,10 +5,7 @@ import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 export default function UserForm({ session }) {
   const supabase = createClientComponentClient();
   const [loading, setLoading] = useState(true);
-  const [firstName, setFirstname] = useState(null);
-  const [lastName, setLastname] = useState(null);
   const [username, setUsername] = useState(null);
-  const [dateOfBirth, setDateOfBirth] = useState(null);
   const user = session?.user;
 
   const getProfile = useCallback(async () => {
@@ -17,7 +14,7 @@ export default function UserForm({ session }) {
 
       let { data, error, status } = await supabase
         .from("users")
-        .select(`name_first, name_last, username, dateofbirth`)
+        .select(`username`)
         .eq("user_id", user?.id)
         .single();
 
@@ -30,10 +27,7 @@ export default function UserForm({ session }) {
       }
 
       if (data) {
-        setFirstname(data.name_first);
-        setLastname(data.name_last);
         setUsername(data.username);
-        setDateOfBirth(data.dateofbirth);
       }
     } catch (error) {
       alert("Error loading user data!");
@@ -47,18 +41,14 @@ export default function UserForm({ session }) {
     getProfile();
   }, [user, getProfile]);
 
-  async function updateProfile({ firstName, lastName, username, dateOfBirth }) {
+  async function updateProfile({ username }) {
     try {
       setLoading(true);
 
       let { error } = await supabase.from("users").upsert({
         user_id: user?.id,
         email: user?.email,
-        name_first: firstName,
-        name_last: lastName,
         username,
-        /* dateofbirth: dateOfBirth,
-         updated_at: new Date().toISOString(),*/
       });
       if (error) throw error;
       alert("User Profile updated!");
@@ -73,22 +63,7 @@ export default function UserForm({ session }) {
   return (
     <div className="form-widget">
       <div>
-        <label htmlFor="firstName">First Name</label>
-        <input
-          id="firstName"
-          type="text"
-          value={firstName || ""}
-          onChange={(e) => setFirstname(e.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="lastName">Last Name</label>
-        <input
-          id="lastName"
-          type="text"
-          value={lastName || ""}
-          onChange={(e) => setLastname(e.target.value)}
-        />
+        <h1 className="text-white text-2xl p-4">Update Username</h1>
       </div>
       <div>
         <label htmlFor="username">Username</label>
@@ -100,20 +75,9 @@ export default function UserForm({ session }) {
         />
       </div>
       <div>
-        <label htmlFor="dateOfBirth">DateOfBirth</label>
-        <input
-          id="dateOfBirth"
-          type="url"
-          value={dateOfBirth || ""}
-          onChange={(e) => setDateOfBirth(e.target.value)}
-        />
-      </div>
-      <div>
         <button
-          className="button primary block"
-          onClick={() =>
-            updateProfile({ firstName, lastName, username, dateOfBirth })
-          }
+          className=" bg-white text-black rounded-xl text-xl p-2 button primary block"
+          onClick={() => updateProfile({ username })}
           disabled={loading}
         >
           {loading ? "Loading ..." : "Update"}
@@ -122,7 +86,10 @@ export default function UserForm({ session }) {
 
       <div>
         <form action="/auth/signout" method="post">
-          <button className="button block" type="submit">
+          <button
+            className="bg-white text-black rounded-xl text-xl p-2 button block"
+            type="submit"
+          >
             Sign out
           </button>
         </form>
