362344c2187993a1b15a9b13a596d294bba9f77d
Ashu Gupta <ashugpt1812@gmail.com>
2024-01-06 23:00:34 +0530

    change in env

--- src/app/auth-form.jsx
+++ src/app/auth-form.jsx
@@ -5,7 +5,7 @@ import { Auth } from "@supabase/auth-ui-react";
 import { ThemeSupa } from "@supabase/auth-ui-shared";
 // import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 
-const BASE_URL = process.env.BASE_URL;
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 const CALLBACK_URL = `${BASE_URL}/auth/callback`;
 
 export default function AuthForm() {
