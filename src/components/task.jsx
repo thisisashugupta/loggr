825245813172f99ade99646b12c2c905297dceff
Ashu Gupta <ashugpt1812@gmail.com>
2024-02-27 00:31:32 +0530

    solved minor ui bugs, added horz scrollbar

--- src/components/task.jsx
+++ src/components/task.jsx
@@ -38,30 +38,29 @@ export default function Task({ taskData, user_id, setTasks, isBookmark }) {
   };
 
   return (
-    <div className="w-[90%] flex justify-center bg-white bg-opacity-30 rounded-lg py-4 overflow-hidden">
-      <div className="flex w-[90%] justify-between items-center space-x-4">
+    <div className="w-full flex justify-left bg-white bg-opacity-30 rounded-lg p-4 overflow-hidden">
+      <div className="w-full flex gap-2 items-center justify-between">
+
         {!isBookmark && (
-          
           <Checkbox 
             className="hover:bg-gray-400"
             checked={isChecked} 
             onCheckedChange={handleCheckboxChange}
             />
-
         )}
 
-        <div className="w-[80%] flex justify-start items-center">
+        <div className="max-w-[260px] flex justify-center items-center">
           {isBookmark ? (
             <a href={isBookmark} target="_blank" >
-              <p className="text-black text-xl hover:text-blue-500">{taskData.title}</p>
+              <p className="text-black text-lg hover:text-blue-500">{taskData.title}</p>
             </a>
           ) : (
-            <p className="text-black text-xl">{taskData.title}</p>
+            <p className="w-full break-words text-black text-lg">{taskData.title}</p>
           )}
         </div>
 
-        <form type="submit" onSubmit={handleDeleteTask}>
-          <button className='flex hover:bg-red-400 p-1 rounded' type='submit'><Trash size={20} strokeWidth={2} /></button>
+        <form className="flex justify-end" type="submit" onSubmit={handleDeleteTask}>
+          <button className='aspect-square hover:bg-red-400 p-1 rounded' type='submit'><Trash size={20} strokeWidth={2} /></button>
         </form>
 
       </div>
